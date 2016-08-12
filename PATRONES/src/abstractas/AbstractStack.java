ff999a65b1b6f68f5c4a50eb94b8d5255eeab157
Carlos <carlos.calderon097@gmail.com>
2016-08-11 23:33:29 -0600

    Se agrego abstractStack

---
+++ PATRONES/src/abstractas/AbstractStack.java
@@ -0,0 +1,37 @@
+package abstractas;
+
+import interfaces.Stack;
+
+public class AbstractStack<E> implements Stack<E> {
+
+	@Override
+	public void push(E item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public E pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean empty() {
+		// TODO Auto-generated method stub
+		return size() == 0;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
